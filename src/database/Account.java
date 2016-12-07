b9bdb07cef83b957a5e4cf486a2d68d6fe362620
GlenLionDew <juchausse@gmail.com>
2016-12-07 11:58:17 +1100

    added method - create and exist for sign up feature

--- src/database/Account.java
+++ src/database/Account.java
@@ -3,6 +3,7 @@ package database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Account {
 
@@ -12,7 +13,33 @@ public class Account {
 		this.conn = conn;
 	}
 
-	
+	public void create(String email, String password) throws SQLException {
+
+	}
+
+	public boolean exist(String email) throws SQLException {
+
+		String sql = "Select count(*) as count from webshop.user where email=?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, email);
+
+		ResultSet rs = stmt.executeQuery();
+
+		int count = 0;
+
+		if (rs.next()) {
+
+			count = rs.getInt("count");
+		}
+
+		if (count == 0) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
 	public boolean login(String email, String password) throws Exception {
 		String sql = "select count(*) as count from webshop.user where email=? and password=?";
 		PreparedStatement stmt = conn.prepareStatement(sql);
